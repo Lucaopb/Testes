35d0111dfb3d32d473b3828b681a0239f3f7867c
Lucaopb <l.piresbrito8@gmail.com>
2024-05-29 10:27:50 -0300

    e2e-dev

---
+++ cypress/e2e/1-getting-started/e2e-Deliv.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress"/>
+
+import { faker } from '@faker-js/faker'
+
+context('Teste - Cadastro site de entregas', () => {
+
+    beforeEach(() => {
+        cy.visit('https://buger-eats.vercel.app/deliver')
+    });
+
+    it('Deve Realizar o cadastro no site', () => {
+        
+
+           
+        
+    });
+    
+});
